acf06acfe994e79bb121aa89d30ae6e63bad4aca
Dawid <105428376+Dawka86@users.noreply.github.com>
2024-10-15 13:07:05 +0100

    security update I added .env

--- server/server.js
+++ server/server.js
@@ -4,20 +4,23 @@ import  {dirname}  from "path";
 import  {join } from "path";
 import { fileURLToPath } from "url";
 import pg from "pg";
-import env from "process";
+import env from "dotenv";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 env.config();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const db = new pg.Client({
-    user: process.env.SECRET_USER,
-    host: process.env.SECRET_HOST,
-    database: process.env.SECRET_DATABASE,
-    password: process.env.SECRET_DATABASE_PASSWORD,
-    port: process.env.SECRET_PORT,
-})
+    user: `${process.env.SECRET_USER}`,
+    host: `${process.env.SECRET_HOST}`,
+    database: `${process.env.SECRET_DATABASE}`,
+    password: `${process.env.SECRET_DATABASE_PASSWORD}`,
+    port: `${process.env.SECRET_PORT}`,
+});
+db.connect();
 
 // Middleware do parsowania danych z formularza
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,8 +37,15 @@ app.get("/", (req, res) => {
 });
 
 // Obsługa formularza POST
-app.post("/submit", (req, res) => {
-  console.log(req.body); 
+app.post("/submit", async(req, res) => {
+  const email = req.body.email;
+  const password = req.body.password; 
+
+  const results = await db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)  RETURNING id",
+    [email, password]
+  );
+  console.log(`Emaile added on new ID :`, results.rows[0].id);
   res.send("Formularz odebrany!");
 });
 
